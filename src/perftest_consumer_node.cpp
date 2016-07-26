35e74cd3962252673b33a8756fdd7f3cdb5d38d7
Konstantin Sorokin <kvs@sigterm.ru>
2016-07-26 23:11:20 +0300

    add queue_size parameter

--- src/perftest_consumer_node.cpp
+++ src/perftest_consumer_node.cpp
@@ -25,6 +25,7 @@ std::ostream& operator<<(std::ostream& stream, const LogEntry& e)
 typedef std::vector<LogEntry> LogEntries;
 
 static const std::string kDefaultLogFile = "/tmp/ros_perftest.log";
+static const int kDefaultQueueSize = 1000;
 
 bool write_log(const std::string &fname, const LogEntries &log_data)
 {
@@ -61,13 +62,18 @@ main(int argc, char **argv)
     ros::NodeHandle priv_nh("~");
 
     std::string log_file;
+    int queue_size;
 
     if (!priv_nh.getParam("log_file", log_file)) {
         ROS_WARN_STREAM("couldn't find 'log_file' configuration parameter, using the default=" << kDefaultLogFile);
         log_file = kDefaultLogFile;
     }
 
-    uint32_t queue_size = 1000;
+    if (!priv_nh.getParam("queue_size", queue_size)) {
+        ROS_WARN_STREAM("couldn't find 'queue_size' configuration parameter, using the default=" << kDefaultQueueSize);
+        queue_size = kDefaultQueueSize;
+    }
+
     ros::TransportHints transport_hints = ros::TransportHints().unreliable(); // UDP transport
 
     LogEntries log_data;
