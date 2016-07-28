5d04e497e97748118600835d039fcb4999c378bb
Konstantin Sorokin <kvs@sigterm.ru>
2016-07-28 16:47:28 +0300

    add Nodelet version

---
+++ src/perftest_consumer_nodelet.cpp
@@ -0,0 +1,143 @@
+#include <fstream>
+
+#include <boost/functional/hash.hpp>
+#include <boost/generator_iterator.hpp>
+#include <boost/thread/thread.hpp>
+
+#include <nodelet/nodelet.h>
+#include <pluginlib/class_list_macros.h>
+
+#include <ros/ros.h>
+#include <unique_id/unique_id.h>
+
+#include "ros_performance_test/TestMessage.h"
+#include "ros_performance_test/data_hash.hpp"
+
+namespace ros_performance_test
+{
+
+struct LogEntry {
+    ros_performance_test::TestMessageHeader incoming;
+    uint32_t data_hash;
+    ros::Time ts;
+};
+
+std::ostream&
+operator<<(std::ostream& stream, const LogEntry& e)
+{
+    stream << e.incoming.uid << "," << e.incoming.seq << "," << e.incoming.data_hash << "," << e.incoming.ts.sec << ","
+           << e.incoming.ts.nsec << ",";
+    stream << e.data_hash << "," << e.ts.sec << "," << e.ts.nsec;
+
+    return stream;
+}
+
+typedef std::vector<LogEntry> LogEntries;
+
+static const std::string kDefaultLogFile = "/tmp/ros_perftest.csv";
+static const int kDefaultQueueSize = 1000;
+static const int kDefaultHashFunctionId = 2;
+
+class PerftestConsumer : public nodelet::Nodelet
+{
+public:
+    PerftestConsumer()
+        : queue_size(0)
+        , hash_function_id(0)
+    {
+        log_data.reserve(10000);
+    }
+
+    ~PerftestConsumer()
+    {
+        write_log();
+    }
+
+private:
+    LogEntries log_data;
+    ros::Subscriber sub;
+    uint16_t uid;
+
+    std::string log_file;
+    int queue_size;
+    int hash_function_id;
+
+    bool
+    write_log()
+    {
+        if (log_file.empty()) {
+            return false;
+        }
+
+        std::fstream result(log_file.c_str(), std::fstream::out);
+
+        const char* headers[] = { "node_id", "sequence_number", "data_hash_on_sender", "ts_sec_sent", "ts_nsec_sent",
+            "data_hash_on_receiver", "ts_sec_received", "ts_nsec_received", NULL };
+
+        int idx = 0;
+        while (headers[idx] != NULL) {
+            if (idx != 0) {
+                result << ",";
+            }
+            result << headers[idx];
+            idx++;
+        }
+        result << std::endl;
+
+        for (size_t i = 0; i < log_data.size(); i++) {
+            result << log_data[i] << std::endl;
+        }
+
+        return true;
+    }
+
+    void
+    data_cb(const ros_performance_test::TestMessage::ConstPtr& msg)
+    {
+        LogEntry e;
+
+        e.incoming = msg->header;
+
+        if (hash_function_id == kDefaultHashFunctionId) {
+            e.data_hash = data_hash_2(msg->data.begin(), msg->data.end());
+        } else if (hash_function_id == 1) {
+            e.data_hash = data_hash_1(msg->data.begin(), msg->data.end());
+        } else {
+            NODELET_ERROR_STREAM("Unknown id of data hash function: " << hash_function_id);
+        }
+
+        e.ts = ros::Time::now();
+
+        log_data.push_back(e);
+    }
+
+    virtual void
+    onInit()
+    {
+        ros::NodeHandle nh = nodelet::Nodelet::getMTNodeHandle();
+        ros::NodeHandle priv_nh = nodelet::Nodelet::getMTPrivateNodeHandle();
+
+        if (!priv_nh.getParam("log_file", log_file)) {
+            NODELET_WARN_STREAM("couldn't find 'log_file' configuration parameter, using the default=" << kDefaultLogFile);
+            log_file = kDefaultLogFile;
+        }
+
+        if (!priv_nh.getParam("queue_size", queue_size)) {
+            NODELET_WARN_STREAM("couldn't find 'queue_size' configuration parameter, using the default=" << kDefaultQueueSize);
+            queue_size = kDefaultQueueSize;
+        }
+
+        if (!priv_nh.getParam("hash_function_id", hash_function_id)) {
+            NODELET_WARN_STREAM("couldn't find 'hash_function_id' configuration parameter, using the default=" << kDefaultHashFunctionId);
+            hash_function_id = kDefaultHashFunctionId;
+        }
+
+        NODELET_INFO_STREAM("Message queue size: " << queue_size);
+
+        sub = nh.subscribe<ros_performance_test::TestMessage>("producer", queue_size, &PerftestConsumer::data_cb, this);
+    }
+};
+
+PLUGINLIB_EXPORT_CLASS(ros_performance_test::PerftestConsumer, nodelet::Nodelet);
+
+} // namespace
---
+++ src/perftest_producer_nodelet.cpp
@@ -0,0 +1,131 @@
+#include <boost/functional/hash.hpp>
+#include <boost/generator_iterator.hpp>
+#include <boost/random.hpp>
+#include <boost/random/uniform_int_distribution.hpp>
+#include <boost/thread/thread.hpp>
+
+#include <nodelet/nodelet.h>
+#include <pluginlib/class_list_macros.h>
+
+#include <ros/ros.h>
+#include <unique_id/unique_id.h>
+
+#include "ros_performance_test/TestMessage.h"
+#include "ros_performance_test/data_hash.hpp"
+
+namespace ros_performance_test
+{
+
+typedef boost::random::uniform_int_distribution<int> UniformDistribution;
+
+static const int kDefaultRate = 10;
+static const int kDefaultPayloadSize = 64;
+static const int kDefaultQueueSize = 1000;
+static const int kDefaultHashFunctionId = 2;
+
+class PerftestProducer : public nodelet::Nodelet
+{
+public:
+    PerftestProducer()
+        : uid(0)
+        , queue_size(0)
+        , rate(0)
+        , payload_size(0)
+        , hash_function_id(0)
+    {
+    }
+
+private:
+    ros::Publisher pub;
+    boost::mt19937 gen;
+    uint16_t uid;
+
+    int queue_size;
+    int rate;
+    int payload_size;
+    int hash_function_id;
+
+    void
+    loop()
+    {
+        ros_performance_test::TestMessagePtr msg(new ros_performance_test::TestMessage());
+        uint32_t seq = 0;
+
+        boost::random::uniform_int_distribution<> small_ints(0, 255);
+        ros::Rate loop_rate(rate);
+
+        while (ros::ok()) {
+            msg->data.clear();
+
+            msg->header.seq = ++seq;
+            for (int i = 0; i < payload_size; i++) {
+                msg->data.push_back(small_ints(gen));
+            }
+
+            if (hash_function_id == kDefaultHashFunctionId) {
+                msg->header.data_hash = data_hash_2(msg->data.begin(), msg->data.end());
+            } else if (hash_function_id == 1) {
+                msg->header.data_hash = data_hash_1(msg->data.begin(), msg->data.end());
+            } else {
+                NODELET_ERROR_STREAM("Unknown id of data hash function: " << hash_function_id);
+            }
+
+            msg->header.uid = uid;
+            msg->header.ts = ros::Time::now();
+
+            pub.publish(msg);
+
+            loop_rate.sleep();
+        }
+
+        NODELET_WARN_STREAM("Exit requested");
+    }
+
+    virtual void
+    onInit()
+    {
+        gen.seed(time(NULL));
+
+        uid = boost::hash_value<boost::uuids::uuid>(unique_id::fromRandom()) % std::numeric_limits<uint16_t>::max();
+
+        ros::NodeHandle nh;
+        ros::NodeHandle priv_nh = nodelet::Nodelet::getMTPrivateNodeHandle();
+
+        gen.seed(time(NULL));
+
+        if (!priv_nh.getParam("rate", rate)) {
+            NODELET_WARN_STREAM("couldn't find 'rate' configuration parameter, using the default=" << kDefaultRate);
+            rate = kDefaultRate;
+        }
+
+        if (!priv_nh.getParam("payload_size", payload_size)) {
+            NODELET_WARN_STREAM("couldn't find 'payload_size' configuration parameter, using the default=" << kDefaultPayloadSize);
+            payload_size = kDefaultPayloadSize;
+        }
+
+        if (!priv_nh.getParam("queue_size", queue_size)) {
+            NODELET_WARN_STREAM("couldn't find 'queue_size' configuration parameter, using the default=" << kDefaultQueueSize);
+            queue_size = kDefaultQueueSize;
+        }
+
+        if (!priv_nh.getParam("hash_function_id", hash_function_id)) {
+            NODELET_WARN_STREAM("couldn't find 'hash_function_id' configuration parameter, using the default=" << kDefaultHashFunctionId);
+            hash_function_id = kDefaultHashFunctionId;
+        }
+
+        uint16_t uid = boost::hash_value<boost::uuids::uuid>(unique_id::fromRandom()) % std::numeric_limits<uint16_t>::max();
+
+        NODELET_INFO_STREAM("Node's ID: " << uid);
+        NODELET_INFO_STREAM("Payload size: " << payload_size);
+        NODELET_INFO_STREAM("Message queue size: " << queue_size);
+        NODELET_INFO_STREAM("Publish rate: " << rate);
+
+        pub = nh.advertise<ros_performance_test::TestMessage>("producer", queue_size);
+
+        boost::thread thr(&PerftestProducer::loop, this);
+    }
+};
+
+PLUGINLIB_EXPORT_CLASS(ros_performance_test::PerftestProducer, nodelet::Nodelet);
+
+} // namespace
